51350e24f3c4ca24c97217a36a719878c0381e9c
ValikKovtun <kovtunvalentyn@gmail.com>
2023-02-17 08:45:23 +0200

    Add project files.

---
+++ Pattern/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Pattern
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 249);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Адаптер";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(189, 249);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 42);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Прототип";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(300, 249);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(176, 42);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Одинак";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(464, 73);
+            this.label1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(464, 69);
+            this.label2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(12, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(464, 95);
+            this.label3.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 303);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Лаб_2";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+    }
+}
---
+++ Pattern/Form1.cs
@@ -0,0 +1,124 @@
+namespace Pattern
+{
+    public partial class Form1 : Form
+    {
+        public interface ITarget
+        {
+            string GetRequest();
+        }
+        class Adaptee
+        {
+            public string GetSpecificRequest()
+            {
+                return "Специфічний запрос.";
+            }
+        }
+        class Adapter : ITarget
+        {
+            private readonly Adaptee _adaptee;
+
+            public Adapter(Adaptee adaptee)
+            {
+                this._adaptee = adaptee;
+            }
+
+            public string GetRequest()
+            {
+                return $"Це був '{this._adaptee.GetSpecificRequest()}'";
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Adaptee adaptee = new Adaptee();
+            ITarget target = new Adapter(adaptee);
+
+            label1.Text = "Інтерфейс адаптера несумісний з клієнтом.";
+            label2.Text = "Але за допомогою адаптера клієнт може викликати його метод.";
+
+            label3.Text = target.GetRequest();
+
+        }
+
+        public class Person
+        {
+            public int Age;
+            public DateTime BirthDate;
+            public string Name;
+            public IdInfo IdInfo;
+
+            public Person ShallowCopy()
+            {
+                return (Person)this.MemberwiseClone();
+            }
+
+            public Person DeepCopy()
+            {
+                Person clone = (Person)this.MemberwiseClone();
+                clone.IdInfo = new IdInfo(IdInfo.IdNumber);
+                clone.Name = this.Name;
+                return clone;
+            }
+        }
+
+        public class IdInfo
+        {
+            public int IdNumber;
+
+            public IdInfo(int idNumber)
+            {
+                this.IdNumber = idNumber;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Person p1 = new Person();
+            p1.Age = 24;
+            p1.BirthDate = Convert.ToDateTime("1999-01-01");
+            p1.Name = "Миша Мішуткін";
+            p1.IdInfo = new IdInfo(111);
+
+            Person p2 = p1.ShallowCopy();
+            Person p3 = p1.DeepCopy();
+
+            label1.Text = "Початкові значення p1, p2:\np1: ";
+            DisplayValues(p1, label1);
+            label1.Text += "\np2:";
+            DisplayValues(p2, label1);
+
+
+            p1.Age = 23;
+            p1.BirthDate = Convert.ToDateTime("2000-01-01");
+            p1.Name = "Діма";
+            p1.IdInfo.IdNumber = 9999;
+            label2.Text = "Значення p1 та p2 після зміни p1:\np1: ";
+            DisplayValues(p1, label2);
+            label2.Text += "\np2:";
+            DisplayValues(p2, label2);
+
+            label3.Text = "Значення p1 та p3:\np1: ";
+            DisplayValues(p1, label3);
+            label2.Text += "\np3:";
+            DisplayValues(p3, label3);
+        }
+
+        public static void DisplayValues(Person p, Label label)
+        {
+            label.Text += "Ім'я: " + p.Name + ", Вік: " + p.Age + ", Народження: " + p.BirthDate;
+            label.Text += "\nID#:" + p.IdInfo.IdNumber;
+        }
+
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
